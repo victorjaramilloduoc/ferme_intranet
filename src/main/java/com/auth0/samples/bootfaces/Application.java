81dc8df7dbbdd11c3657102ff14b606a4624dcf2
Victor Jaramillo <vjaramillo7@gmail.com>
2020-04-14 14:53:50 -0400

    Solución de compilado

--- src/main/java/com/auth0/samples/bootfaces/Application.java
+++ src/main/java/com/auth0/samples/bootfaces/Application.java
@@ -1,20 +1,22 @@
 package com.auth0.samples.bootfaces;
 
+import java.util.EnumSet;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.DispatcherType;
+
 import org.ocpsoft.rewrite.servlet.RewriteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-import javax.faces.webapp.FacesServlet;
-import javax.servlet.DispatcherType;
-import java.util.EnumSet;
-
 @EnableAutoConfiguration
 @ComponentScan({"com.auth0.samples.bootfaces"})
+@SuppressWarnings({"rawtypes", "unchecked"})
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
