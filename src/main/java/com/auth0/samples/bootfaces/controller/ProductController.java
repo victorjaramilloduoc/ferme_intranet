937152711aeba30f2e2ce2a37c6ff107f1a1a6db
Elias <elias.cejeb@gmail.com>
2020-04-24 00:32:29 -0300

    Cambio calendario.

--- src/main/java/com/auth0/samples/bootfaces/controller/ProductController.java
+++ src/main/java/com/auth0/samples/bootfaces/controller/ProductController.java
@@ -4,12 +4,21 @@ import org.ocpsoft.rewrite.annotation.Join;
 import org.ocpsoft.rewrite.el.ELBeanName;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import java.util.Date;
 
 @Scope(value = "session")
 @Component(value = "productController")
 @ELBeanName(value = "productController")
 @Join(path = "/new-product", to = "/product/product-form.jsf")
 public class ProductController {
-	
 
+	private Date date = new Date();
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
 }
