82394dd522d3104fab223160deb78448aaaa39b2
Victor Jaramillo <vjaramillo7@gmail.com>
2020-04-27 00:53:55 -0400

    Nuevas fuciones

--- src/main/java/com/auth0/samples/bootfaces/controller/ListUsersController.java
+++ src/main/java/com/auth0/samples/bootfaces/controller/ListUsersController.java
@@ -4,10 +4,9 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.faces.application.FacesMessage;
-import javax.faces.application.FacesMessage.Severity;
-import javax.faces.context.FacesContext;
 
 import org.json.JSONArray;
 import org.ocpsoft.rewrite.annotation.Join;
@@ -15,7 +14,6 @@ import org.ocpsoft.rewrite.annotation.RequestAction;
 import org.ocpsoft.rewrite.el.ELBeanName;
 import org.ocpsoft.rewrite.faces.annotation.Deferred;
 import org.ocpsoft.rewrite.faces.annotation.IgnorePostback;
-import org.primefaces.PrimeFaces;
 import org.primefaces.event.RowEditEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,6 +21,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import com.auth0.samples.bootfaces.util.FacesUtil;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.portafolio.util.entities.UserEntity;
@@ -30,31 +29,33 @@ import com.portafolio.util.rest.client.RestClientUtil;
 
 import lombok.Data;
 
-@Scope (value = "session")
-@Component (value = "listUsers")
+@Scope(value = "session")
+@Component(value = "listUsers")
 @ELBeanName(value = "listUsers")
 @Join(path = "/users", to = "/user/user-list.jsf")
 @Data
 public class ListUsersController {
-	
+
 	private Logger LOG = LoggerFactory.getLogger(ListUsersController.class);
-	
+
 	private List<UserEntity> listUsers = new ArrayList<>();
-	
+
 	@Value("${local.connection.timeout}")
 	private Integer connectionTimeout;
 
 	@Value("${local.connection.request.timeout}")
 	private Integer requestTimeout;
-	
+
 	@Value("${local.read.timeout}")
 	private Integer readTimeout;
-	
+
 	@Value("${local.retry.endpoint}")
 	private Integer retryEndPoint;
-	
+
 	@Value("${service.url.ferme.users}")
 	private String usersUrl;
+	
+	private Long idUserToDisable;
 
 	@Deferred
 	@RequestAction
@@ -63,50 +64,47 @@ public class ListUsersController {
 		JSONArray json = RestClientUtil.getJsonArrayFromWs(usersUrl, null, null, null, buildPropertiesMap());
 
 		Gson gson = new Gson();
-		listUsers = gson.fromJson(json.toString(), new TypeToken<List<UserEntity>>(){}.getType());
-		LOG.info("{}",listUsers);
+		listUsers = gson.fromJson(json.toString(), new TypeToken<List<UserEntity>>() {
+		}.getType());
+		listUsers = listUsers.stream().filter(user -> user.isEnable()).collect(Collectors.toList());
+		LOG.info("{}", listUsers);
 	}
-	
+
 	public void onRowEdit(RowEditEvent event) {
 		System.out.println("Row edited");
-		System.out.println((UserEntity)event.getObject());
-		
-		Object response = RestClientUtil.putToWs(usersUrl, null, null, (UserEntity)event.getObject(), null);
-		if(response != null) {
-			System.out.println("Respuesta: "+response);
-			
-			showPopUpMessage(FacesMessage.SEVERITY_INFO, "Inforcación", "Fila editada");
-		}else {
+		System.out.println((UserEntity) event.getObject());
+
+		Object response = RestClientUtil.putToWs(usersUrl, null, null, (UserEntity) event.getObject(), null);
+		if (response != null) {
+			System.out.println("Respuesta: " + response);
+
+			FacesUtil.showPopUpMessage(FacesMessage.SEVERITY_INFO, "Inforcación", "Fila editada");
+		} else {
 			loadData();
-			showPopUpMessage(FacesMessage.SEVERITY_ERROR, "Inforcación", "Edición fallida, error en servicio");
+			FacesUtil.showPopUpMessage(FacesMessage.SEVERITY_ERROR, "Error", "Edición fallida, error en servicio");
 		}
-    }
-     
-    public void onRowCancel(RowEditEvent event) {
-    	System.out.println("Edit cancel");
-        System.out.println((UserEntity)event.getObject());
-        
-        showPopUpMessage(FacesMessage.SEVERITY_INFO, "Error", "Edición cancelada");
-    }
+	}
+
+	public void onRowCancel(RowEditEvent event) {
+		System.out.println("Edit cancel");
+		System.out.println((UserEntity) event.getObject());
+
+		FacesUtil.showPopUpMessage(FacesMessage.SEVERITY_INFO, "Info", "Edición cancelada");
+	}
 	
-	private Map<String, Integer> buildPropertiesMap(){
-		Map<String,Integer> response = new LinkedHashMap<>();
+	public void disableUser() {
+		System.out.println(idUserToDisable);
+		FacesUtil.closePopUp("confirmDialog");
+		FacesUtil.showPopUpMessage(FacesMessage.SEVERITY_INFO, "Info", "Usuario Id: "+idUserToDisable+" deshabilitado.");
+	}
+
+	private Map<String, Integer> buildPropertiesMap() {
+		Map<String, Integer> response = new LinkedHashMap<>();
 		response.put("connectionTimeout", connectionTimeout);
 		response.put("requestTimeout", requestTimeout);
 		response.put("readTimeout", readTimeout);
 		response.put("retryEndPoint", retryEndPoint);
 		return response;
-	}
-	
-	public void showMessage(String titleMsg, String bodyMsg) {
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, titleMsg, bodyMsg);
-         
-        PrimeFaces.current().dialog().showMessageDynamic(message);
-    }
-	
-	private void showPopUpMessage(Severity messageType, String title, String body) {
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(messageType,
-				title, body));
 	}
 
 }
---
+++ src/main/java/com/auth0/samples/bootfaces/util/FacesUtil.java
@@ -0,0 +1,39 @@
+package com.auth0.samples.bootfaces.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.PrimeFaces;
+
+public class FacesUtil {
+	
+	/**
+	 * 
+	 * @param wid
+	 */
+	public static void openPopUp(String wid) {
+		PrimeFaces current = PrimeFaces.current();
+		current.executeScript("PF('"+wid+"').show();");
+	}
+	
+	/**
+	 * 
+	 * @param wid
+	 */
+	public static void closePopUp(String wid) {
+		PrimeFaces current = PrimeFaces.current();
+		current.executeScript("PF('"+wid+"').hide();");
+	}
+	
+	public static void showMessage(String titleMsg, String bodyMsg) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, titleMsg, bodyMsg);
+
+		PrimeFaces.current().dialog().showMessageDynamic(message);
+	}
+	
+	public static void showPopUpMessage(Severity messageType, String title, String body) {
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(messageType, title, body));
+	}
+
+}
